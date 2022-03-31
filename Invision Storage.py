f3c5022fd313d7c2667c4fc32af15e3a7d1b7661
Clayton Silva dos Santos <clayton_box@outlook.com>
2022-03-30 23:43:22 -0300

    the work has been started

--- Invision Storage.py
+++ Invision Storage.py
@@ -3,11 +3,6 @@ Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 # 0 being off 1 being on as in true / false
 # you can use 0 or 1 && True or False
 
-Config.set('graphics', 'resizable', '0')
-Config.set('graphics', 'width', '1200')
-Config.set('graphics', 'height', '660')
-
-
 
 from kivy.uix.screenmanager import ScreenManager, Screen
 import pickle
@@ -43,7 +38,12 @@ import webbrowser
 from kivymd.app import MDApp
 from kivymd.uix.button import MDFlatButton
 from kivymd.uix.dialog import MDDialog
-
+from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelThreeLine, MDExpansionPanelOneLine
+from kivymd import images_path
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scrollview import ScrollView
+from kivymd.uix.label import MDLabel
+from kivy.animation import Animation
 
 class Tema():
 	def __init__(self, cor_aplicativo, cor_aplicativo_tuple, cor_fundo_trabalho, cor_fundo_trabalho_tuple, cor_widget, cor_widget_hover, cor_fundo_menu_deg1, cor_fundo_menu_deg2, cor_letra_menu, cor_licenca, cor_licenca_texto):
@@ -73,10 +73,64 @@ def read_or_new_pickle(path, default):
 class ButtonGrid(ButtonBehavior, MDBoxLayout, HoverBehavior):
 	pass
 
+class Content(BoxLayout):
+    '''Custom content.'''
+
+
+class Menu_Botao(ButtonBehavior, HoverBehavior, MDBoxLayout):
+	cor = StringProperty("323335")
+	def __init__(self,**kwargs):
+		super().__init__( **kwargs)
+		self.md_bg_color = get_color_from_hex("#{}".format(self.cor))
+
+	def animate_enter(self, widget, *args):
+		animate = Animation(md_bg_color= get_color_from_hex("#656667"), duration=0.25)
+		animate.start(widget)
+
+	def animate_leave(self, widget, *args):
+		animate = Animation(md_bg_color= get_color_from_hex("#323335"), duration=0.25)
+		animate.start(widget)
+
+	def animate_press(self, widget, *args):
+		animate = Animation(md_bg_color= get_color_from_hex("#b92034"), duration=0.2)
+		animate.start(widget)
+
+	def animate_release(self, widget, *args):
+		animate = Animation(md_bg_color= get_color_from_hex("#323335"), duration=0.4)
+		animate.start(widget)
+
+
+	def on_press(self):
+		self.animate_press(self)
+
+	def on_release(self):
+		def delay(dt):
+			self.animate_release(self)
+		Clock.schedule_once(delay, 0.2)
+
+	def on_enter(self):
+		self.animate_enter(self)
+
+	def on_leave(self):
+		self.animate_leave(self)
+
+class Inicio(Screen):
+	def animate_rotate(self, widget, *args):
+		animate = Animation(rotation=360)
+		animate.start(widget)
 
 
 
 class Inicio(Screen):
+	pass
+
+class Sell(Screen):
+	pass
+
+class Storage(Screen):
+	pass
+
+class IA(Screen):
 	pass
 
 class WindowManager(ScreenManager):
@@ -90,6 +144,11 @@ class InvisionStorage(MDApp):
 		self.title = 'Invision Storage'
 		self.icon = 'icons\icon.png'
 		return Builder.load_file('InvisionStorage.kv')
+
+	#Window.borderless = True
+	Window.maximize()
+
+	#Window.custom_titlebar = True
 
 
 
